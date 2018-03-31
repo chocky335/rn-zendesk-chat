0f4574b77991b3deedb85f7ac7f1e36c95a8c11e
Bohdan Pomohaibo <bohdan.pomohaibo@gmail.com>
2018-03-31 03:13:49 +0200

    Migrate ios base from ObjC to Swift files

---
+++ ios/ZendeskChatSDK/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ZendeskChatSDK
+//
+//  Created by Bogdan Pomogaibo on 3/31/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  var bridge: RCTBridge!
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL
+    
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "ZendeskChatSDK", initialProperties: nil, launchOptions: launchOptions)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
