e5b8629755a6ccc7eba44492c7ed68a43923c730
Bohdan Pomohaibo <bohdan.pomohaibo@gmail.com>
2018-04-01 07:18:49 +0200

    Update testing app

--- App.js
+++ App.js
@@ -1,58 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// @flow
 
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  NativeModules,
+  TouchableOpacity,
+} from "react-native"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import ZendeskChatSDK from "./src/sdk"
 
-type Props = {};
+
+ZendeskChatSDK.setLoggerMode("DEBUG")
+const { ZendeskChatManager } = NativeModules
+
+window.test = ZendeskChatManager
+
+type Props = {}
+
+const colors = {
+  darkGray: "#333333",
+  lightGray: "#eee",
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  btn: {
+    width: 100,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.lightGray,
+    borderRadius: 3,
+    marginBottom: 5,
+  },
+  btnText: {
+    textAlign: "center",
+    color: colors.darkGray,
+  },
+})
+
 export default class App extends Component<Props> {
+  setKey = () => {
+    ZendeskChatSDK.init("583BgUYka2R2mchvfE4G1dXvzobd2V4D")
+  }
+  setUser = () => {
+    const user = {
+      name: "JILZEEE",
+      email: "email@mail.com",
+      phone: "+3561234567",
+      shouldPersist: true,
+    }
+
+    ZendeskChatSDK.setUser(user)
+  }
+  start = async () => {
+    ZendeskChatSDK.openChatModal()
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+
+        <TouchableOpacity style={styles.btn} onPress={this.setKey}>
+          <Text style={styles.btnText}>
+            setKey
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={this.setUser}>
+          <Text style={styles.btnText}>
+            setUser
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={this.start}>
+          <Text style={styles.btnText}>
+            startChat
+          </Text>
+        </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
